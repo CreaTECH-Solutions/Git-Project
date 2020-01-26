93244dea26471fcabba98ded38c3fbb92cce6d13
Stewart Lynch <slynch@createchsol.com>
2020-01-26 12:24:49 -0800

    Initial Commit

---
+++ Git Project/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Git Project
+//
+//  Created by Stewart Lynch on 2020-01-26.
+//  Copyright © 2020 CreaTECH Solutions. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isPresented = false
+    var body: some View {
+        NavigationView {
+            Button("Show Modal") {
+                self.isPresented = true
+            }.sheet(isPresented: $isPresented) {
+                Text("This is the modal screen")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
