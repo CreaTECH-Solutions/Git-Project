9b42b0dec7eaab9c1da7ecff9af415e13da5afa6
Stewart Lynch <slynch@createchsol.com>
2020-01-26 12:29:05 -0800

    Created separate ModalView

---
+++ Git Project/ModalView.swift
@@ -0,0 +1,21 @@
+//
+//  ModalView.swift
+//  Git Project
+//
+//  Created by Stewart Lynch on 2020-01-26.
+//  Copyright © 2020 CreaTECH Solutions. All rights reserved.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text("This is the modal View!")
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
